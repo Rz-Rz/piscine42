1cf3e4393b587dba2c3b55c3122f14a9d9482419
Karim DHRIF <kdhrif@e3r5p10.clusters.42paris.fr>
2022-02-17 12:11:37 +0100

    my strlcat was deleted

--- C_04/ex05/ft_atoi_base.c
+++ C_04/ex05/ft_atoi_base.c
@@ -6,20 +6,13 @@
 /*   By: kdhrif <kdhrif@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 09:47:09 by kdhrif            #+#    #+#             */
-/*   Updated: 2022/02/16 09:47:10 by kdhrif           ###   ########.fr       */
+/*   Updated: 2022/02/16 18:09:58 by kdhrif           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int ft_atoi_base(char *str, char *base);
 
-int     ft_isneg(int neg, int nb)
-{
-		if (neg % 2 != 0)
-				nb *= -1;
-		return (nb);
-}
-
-int is_inbase(char c, char *base)
+int ft_basewalk(char c, char *base)
 {
 		int i;
 
@@ -27,8 +20,9 @@ int is_inbase(char c, char *base)
 		while (base[++i])
 		{
 				if (c == base[i])
+                    return (i);
 		}
-
+    return (0);
 }
 
 int ft_baselen(char *base)
@@ -38,43 +32,64 @@ int ft_baselen(char *base)
 		i = 0;
 		while (base[i])
 		{
-				if (base[i] =='+' || base[i] =='-')
-						return 0;
-				else if (base[i] < 32 && base[i] >= 0)
-						return 0;
-				else if ()
+            if (base[i] == '+' || base[i] == '-')
+                return (0);
+            else if (base[i] < 32 && base[i] >= 0)
+                return (0);
+            else if (ft_basewalk(base[i], base + i + 1))
+                return (0);
 
+            i++;
 		}
-		i++;
 		return (i);
 }
 
-int     ft_atoi(char *str)
+int ft_atoi_base(char *str, char *base)
 {
 		int     nb;
 		int     neg;
-		char arr;
+        int     i;
+        int size;
 
-		while (*str)
-		{
-				while ((*str >= 9 && *str <= 13) || (*str == ' '))
-						str++;
-				if ((*str >= 33 && *str <= 126) || *str == '-' || *str == '+')
-				{
-						while (*str == '-' || *str == '+')
-						{
-								if (*str == '-')
-										neg++;
-								str++;
-						}
-						while (*str >= 33 && *str <= 126)
-						{
-								arr[i++] = *str; 
-								str++;
-						}
-						break ;
-				}
-				str++;
-		}
+        neg = 0;
+        nb = 0;
+        i = 0;
+        if ((size = ft_baselen(base)) < 2) 
+            return (0);
+        while ((*str >= 9 && *str <= 13) || (*str == ' '))
+            str++;
+        while (*str == '-' || *str == '+')
+        {
+            if (*str == '-')
+                neg++;
+            str++;
+        }
+        if ((*str >= 9 && *str <= 13) || (*str == ' '))
+            return (0);
+        while (*str)
+        {
+            i = ft_basewalk(*str, base);
+            nb = nb * size + i;
+            str++;
+        }
+        if (neg % 2 == 1)
+            nb *= -1;
 		return (nb);
 }
+/*
+#include <stdio.h>
+
+
+int	main(void)
+{
+	printf("42:%d\n", ft_atoi_base("2a", "0123456789abcdef"));
+	printf("-42:%d\n", ft_atoi_base("   --------+-2a", "0123456789abcdef"));
+	printf("42:%d\n", ft_atoi_base("   -+-2a", "0123456789abcdef"));
+	printf("0:%d\n", ft_atoi_base("   --------+- 2a", "0123456789abcdef"));
+	printf("0:%d\n", ft_atoi_base("   --------+-z", "0123456789abcdef"));
+	printf("0:%d\n", ft_atoi_base("   --------+-2a", ""));
+	printf("0:%d\n", ft_atoi_base("   --------+-2a", "0"));
+	printf("0:%d\n", ft_atoi_base("   --------+-2a", "+-0"));
+	printf("0:%d\n", ft_atoi_base("   --------+-2a", "\t01"));
+}
+*/
--- C_05/ex07/ft_find_next_prime.c
+++ C_05/ex07/ft_find_next_prime.c
@@ -6,29 +6,58 @@
 /*   By: kdhrif <kdhrif@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 08:33:33 by kdhrif            #+#    #+#             */
-/*   Updated: 2022/02/17 08:33:34 by kdhrif           ###   ########.fr       */
+/*   Updated: 2022/02/17 12:07:10 by kdhrif           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-int ft_find_next_prime(int nb)
+int	ft_is_prime(int nb)
 {
-		int     i;
-		int j;
+	int	i;
 
-		if (nb <= 0 || nb == 1)
-				return (0);
-		i = 2;
-		j = 0;
-		while (i <= nb) {
-
-				while ()
-				{
-						if (nb + j % 2 == 0)
-								return (0);
-						i++;
-				}
-		}
-		return (1);
+	if (nb <= 0 || nb == 1)
+		return (0);
+	i = 2;
+	while (i <= (nb / 2))
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
+int	ft_find_next_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 2)
+		return (2);
+	i = 0;
+	while (ft_is_prime(i + nb) == 0)
+		i++;
+	return (i + nb);
+}
+/*
+#include <stdio.h>
+
+int	main(void    )
+{
+    printf("%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n",
+            ft_find_next_prime(-1) == 2 ? "OK" : "Fail",
+            ft_find_next_prime(-3) == 2 ? "OK" : "Fail",
+            ft_find_next_prime(0) == 2 ? "OK" : "Fail",
+            ft_find_next_prime(1) == 2 ? "OK" : "Fail",
+            ft_find_next_prime(2) == 2 ? "OK" : "Fail",
+            ft_find_next_prime(3) == 3 ? "OK" : "Fail",
+            ft_find_next_prime(4) == 5 ? "OK" : "Fail",
+            ft_find_next_prime(5) == 5 ? "OK" : "Fail",
+            ft_find_next_prime(6) == 7 ? "OK" : "Fail",
+            ft_find_next_prime(7) == 7 ? "OK" : "Fail",
+            ft_find_next_prime(10) == 11 ? "OK" : "Fail",
+            ft_find_next_prime(11) == 11 ? "OK" : "Fail",
+            ft_find_next_prime(13) == 13 ? "OK" : "Fail",
+            ft_find_next_prime(19) == 19 ? "OK" : "Fail",
+            ft_find_next_prime(20) == 23 ? "OK" : "Fail"
+          );
+}
+*/
