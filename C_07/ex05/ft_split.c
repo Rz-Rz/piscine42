9150bf1a6fdfb4c89e1e07bb393ae03d3799daf5
Karim DHRIF <kdhrif@e3r9p9.clusters.42paris.fr>
2022-02-23 15:25:48 +0100

    mega bogoss ft split

--- C_07/ex05/ft_split.c
+++ C_07/ex05/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: kdhrif <kdhrif@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 10:13:50 by kdhrif            #+#    #+#             */
-/*   Updated: 2022/02/23 10:13:52 by kdhrif           ###   ########.fr       */
+/*   Updated: 2022/02/23 15:17:29 by kdhrif           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,80 +14,77 @@
 
 int ft_sep(char c, char *charset)
 {
-		int i;
+    int i;
 
-		i = -1;
-		while(charset[++i])
-				if (c == charset[i])
-						return(1);
-		return (0);
+    i = -1;
+    while(charset[++i])
+        if (c == charset[i])
+            return(1);
+    return (0);
 
 }
-
-int ft_wl(char *str, char *charset)
-{
-		int i;
-		int c;
-
-		i = 0;
-		c = 0;
-		while (str[i])
-		{
-				while (ft_sep(str[i], charset))
-						i++;
-				while (!(ft_sep(str[i], charset) && str[i]))
-				{
-						c++;
-						i++;
-				}
-				if ((ft_sep(str[i], charset) || str[i] == '\0') && !(ft_sep(str[i - 1], charset)))
-						return (c);
-				i++;
-		}
-		return (0);
-}
-
 
 int ft_cw(char *str, char *charset)
 {
-		int i;
-		int j;
-		int w;
+    int i;
+    int w;
 
-		i = 0;
-		w = 0;
-		while (str[i])
-		{
-				while (ft_sep(str[i], charset))
-						i++;
-				while (!(ft_sep(str[i], charset)) && str[i])
-						i++;
-				if (ft_sep(str[i], charset) && !(ft_sep(str[i - 1], charset)))
-						w++;
-				i++;
-		}
-		return (w);
+    i = 0;
+    w = 0;
+    while (str[i])
+    {
+        while (ft_sep(str[i], charset))
+            i++;
+        while (!(ft_sep(str[i], charset)) && str[i])
+            i++;
+        if (ft_sep(str[i], charset) && !(ft_sep(str[i - 1], charset)))
+            w++;
+        i++;
+    }
+    return (w);
+}
+
+char *ft_strdup(char *src, char *charset)
+{
+    int i;
+    char *dest;
+
+    i = 0;
+    while(src[i] && !(ft_sep(src[i], charset)))
+        i++;
+    dest = malloc(sizeof(char) * (i + 1));
+    if (!dest)
+        return (NULL);
+    i = -1;
+    while (src[++i])
+        dest[i] = src[i];
+    dest[i] = '\0';
+    return (dest);
 }
 
 char **ft_split(char *str, char *charset)
 {
-		int i;
-		int j;
-		int w;
-		char **dest;
+    int i;
+    int j;
+    int w;
+    char **dest;
 
 
-		w = ft_cw(str, charset);
-		*dest = malloc(sizeof(char*) * (w + 1));
+    w = ft_cw(str, charset);
+    dest = malloc(sizeof(char*) * (w + 1));
+    if (!dest)
+        return (NULL);
 
-		i = -1;
-		while (dest[++i])
-		{
-
-				
-				dest[i] = malloc();
-				j = 0;
-
-
-		}
+    i = -1;
+    j = 0;
+    while (dest[++i])
+    {
+        while (ft_sep(str[j], charset))
+            j++;
+        if (!(ft_sep(str[j], charset)) && str[j])
+            dest[i] = ft_strdup(&str[j], charset);
+        while (!(ft_sep(str[j], charset)) && str[j])
+            j++;
+    }
+    return (dest);
 }
